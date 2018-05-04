ca545d68b902aa84f71e5dd80da2744560a23cf4
Kyle Morton <kylemortondevelopment@gmail.com>
2018-05-04 09:15:29 -0500

    completed module 4 - dynamic loading

--- DynamicLoading/PeopleViewer.Test/MainViewModelTest.cs
+++ DynamicLoading/PeopleViewer.Test/MainViewModelTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,5 +8,28 @@ namespace PeopleViewer.Test
     [TestClass]
     public class MainViewModelTest
     {
+
+        [TestMethod]
+        public void People_OnFetchData_IsPopulated()
+        {
+            //arrange
+            var vm = new MainViewModel();
+
+            //act
+            vm.FetchData();
+
+            //assert
+            Assert.IsNotNull(vm.People);
+            Assert.AreEqual(2, vm.People.Count());
+        }
+
+        [TestMethod]
+        public void RepositoryType_OnCreation_ReturnsFakeRepositoryString()
+        {
+            var vm = new MainViewModel();
+            var expectedString = "PersonRepository.Fake.FakeRepository";
+
+            Assert.AreEqual(expectedString, vm.RepositoryType);
+        }
     }
 }
---
+++ DynamicLoading/PersonRepository.Fake/FakeRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonRepository.Interface;
+
+namespace PersonRepository.Fake
+{
+    public class FakeRepository : IPersonRepository
+    {
+        public IEnumerable<Person> GetPeople()
+        {
+            var people = new List<Person>
+            {
+                new Person
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Rating = 7,
+                    StartDate = DateTime.Now.AddDays(7)
+                },
+                new Person
+                {
+                    FirstName = "Mary",
+                    LastName = "Thomas",
+                    Rating = 9,
+                    StartDate = DateTime.Now.AddYears(-1)
+                }
+            };
+            return people;
+        }
+
+        public Person GetPerson(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPerson(Person newPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePerson(string lastName, Person updatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePerson(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePeople(IEnumerable<Person> updatedPeople)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
